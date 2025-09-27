de4f18ae6fe02f2bd9204370288f521e3030bf9a
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2025-09-27 18:16:49 -0500

    add search icon

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -10,6 +10,11 @@ import { Component } from '@angular/core';
       method="GET"
       autocomplete="off"
     >
+      <div class="search-icon">
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </div>
       <input
         type="text"
         name="q"
